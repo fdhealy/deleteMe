de349fbf6caba3b95dd3148c14a2e2b1e8a5a12c
vfSleeth <michael.sleeth@ttec.com>
2021-09-05 21:05:27 -0500

    breaks out db accessor

--- franco_lambda.py
+++ franco_lambda.py
@@ -2,27 +2,27 @@ from __future__ import print_function
 import boto3
 import json
 import datetime
+
 dt = datetime.datetime
+
 
 def getUserBday(bday):
     now = dt.now()
-    userDateParts = bday.split('/')
+    userDateParts = bday.split("/")
     userYr = userDateParts[0]
     userMo = userDateParts[1]
     userDy = userDateParts[2]
 
-    timeLeft = dt(year = userYr, month = userMo, day = userDy) - dt(year=now.year, month=now.month, day=now.day)
-    ds = timeLeft.days*24*60*60*10^9
-    print (ds+365*24*60*60*10^9)
+    timeLeft = dt(year=userYr, month=userMo, day=userDy) - dt(
+        year=now.year, month=now.month, day=now.day
+    )
+    ds = timeLeft.days * 24 * 60 * 60 * 10 ^ 9
+    print(ds + 365 * 24 * 60 * 60 * 10 ^ 9)
 
     # return ds+365*24*60*60*10^9
 
-def handler(event, context):
 
-    userBday = getUserBday(event['bday'])
-
-
-    print("Received event: " + json.dumps(event, indent=2))
+def handleDb(operation, data):
     operation = event["potato"]
     mahinfo = event["steak"]
     dynamo = boto3.resource("dynamodb")
@@ -37,12 +37,20 @@ def handler(event, context):
         "echo": lambda x: x,
         "ping": lambda x: "pong",
     }
-    
-    
+
     if operation in operations:
         print("operation = ", operation)
-        
+
         # TODO: Use try catch, handle response code != 200
         return operations[operation](mahinfo)
     else:
         raise ValueError('Unrecognized operation "{}"'.format(operation))
+
+
+def handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    if event["bday"]:
+        userBday = getUserBday(event["bday"])
+        handleDb("update", event["bday"])  # pass smg like dis'
+        return userBday
